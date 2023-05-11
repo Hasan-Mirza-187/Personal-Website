48a055e5e9b292d3b7232c3d3f05ca4f146ab228
Hasan Mumtaz Mirza <hasanmirza_187@yahoo.com>
2023-05-11 20:18:39 +0500

    first upload

---
+++ personal-website/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl font-bold underline justify-center">
+          Hasan Mumtaz Mirza
+        </h1>
+        <h2>Welcome to my personal website.</h2>
+      </div>
+    </div>
+  );
+}
