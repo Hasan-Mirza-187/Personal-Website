d05b2a3acab670c609ad26c093caad216742e91e
Hasan Mumtaz Mirza <hasanmirza_187@yahoo.com>
2023-05-20 00:33:10 +0500

    first version

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -4,31 +4,40 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div>
-        <h1 className="text-3xl font-bold text-center">Hasan Mumtaz Mirza</h1>
+      <div className="flex space-x-4 items-center justify-center bg-slate-800 h-20">
+        <div>
+          <Link className="font-bold text-white bg-slate-800" href="/about">
+            About Me
+          </Link>
+        </div>
+        <div>
+          <Link className="font-bold text-white bg-slate-800" href="/services">
+            Services
+          </Link>
+        </div>
+        <div>
+          <Link
+            className="font-bold text-white bg-slate-800"
+            href="/contact-us"
+          >
+            Contact Us
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-col h-60 w-screen text-white bg-slate-600 justify-center items-center">
+        <h1 className="text-3xl  font-bold text-center">Hasan Mumtaz Mirza</h1>
+        <div>
+          <Image
+            className="rounded-full h-[8rem] w-[8rem] mt-4"
+            src="/profile-pic.jpg"
+            width={150}
+            height={150}
+            alt="Picture of the author"
+          />
+        </div>
         <h2 className="font-bold text-center pt-6">
           Welcome to my personal website!
         </h2>
-      </div>
-
-      <div>
-        <div className="grid grid-flow-col auto-cols-max gap-4 place-content-center h-20 place-items-center">
-          <div>
-            <Link className="font-bold bg-green-700" href="/about">
-              About Me
-            </Link>
-          </div>
-          <div>
-            <Link className="font-bold bg-rose-700" href="/services">
-              Services
-            </Link>
-          </div>
-          <div>
-            <Link className="font-bold bg-blue-700" href="/contact-us">
-              Contact Us
-            </Link>
-          </div>
-        </div>
       </div>
     </div>
   );
