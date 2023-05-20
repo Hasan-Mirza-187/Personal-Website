274d6924fbe8ef99716dc755481904058e77f5a0
Hasan Mumtaz Mirza <hasanmirza_187@yahoo.com>
2023-05-20 14:31:22 +0500

    with navbar

--- personal-website/src/app/layout.tsx
+++ personal-website/src/app/layout.tsx
@@ -1,21 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "./navBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Hasan Mumtaz Mirza',
-  description: 'Created by Hasanmirza',
-}
+  title: "Hasan Mumtaz Mirza",
+  description: "Created by Hasanmirza",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -1,37 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 
+
 export default function Home() {
   return (
-    <div>
-      <div className="flex space-x-4 items-center justify-center bg-slate-800 h-20">
-        <div>
-          <Link className="font-bold text-white bg-slate-800" href="/about">
-            About Me
-          </Link>
-        </div>
-        <div>
-          <Link className="font-bold text-white bg-slate-800" href="/services">
-            Services
-          </Link>
-        </div>
-        <div>
-          <Link
-            className="font-bold text-white bg-slate-800"
-            href="/contact-us"
-          >
-            Contact Us
-          </Link>
-        </div>
-      </div>
-      <div className="flex flex-col h-60 w-screen text-white bg-slate-600 justify-center items-center">
+    <div className="main_div flex">
+      
+      <div className="flex flex-col h-screen w-screen text-white bg-slate-600 justify-center items-center">
         <h1 className="text-3xl  font-bold text-center">Hasan Mumtaz Mirza</h1>
         <div>
           <Image
-            className="rounded-full h-[8rem] w-[8rem] mt-4"
-            src="/profile-pic.jpg"
-            width={150}
-            height={150}
+            className=" mt-6 h-auto w-fit"
+            src="/pic-2.jpg"
+            width={1500}
+            height={1500}
             alt="Picture of the author"
           />
         </div>
